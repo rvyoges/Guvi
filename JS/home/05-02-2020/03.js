f32129dfe9154a437a346ad50ba00d6032f18832
rvyoges <rvyogeswaran23@icloud.com>
2020-02-06 07:30:35 +0530

    Create 03.js

---
+++ JS/home/05-02-2020/03.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Web Page Design</title>
+<script>
+    var array = [1, 2, 3, 4, 5];
+    var sum = array.reduce(function(a, b){
+        return a + b;
+    }, 0);
+    alert(sum);
+</script>
+</head>
+<body>
+</body>
+</html>
