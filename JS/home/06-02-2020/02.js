5749551f5c245a1610bcfe31492aa92efc0b7c42
rvyoges <rvyogeswaran23@icloud.com>
2020-02-07 07:32:24 +0530

    Create 02.js

---
+++ JS/home/06-02-2020/02.js
@@ -0,0 +1,8 @@
+/*Write a function called “printAllKeys” which returns an newArray of all the input object’s keys.*/
+
+var obj = {name : 'RajiniKanth', age : 25, hasPets : true};
+function printAllKeys(obj) {
+console.log(Object.keys(obj))
+console.log(Object.values(obj))
+}
+printAllKeys(obj)
